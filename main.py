9e6940dc724db5715b40e716cf49c12453768e56
Md. Emran Jahan <emranjahan7@gmail.com>
2022-07-18 20:44:10 +0600

    Capture Video and show it

---
+++ main.py
@@ -0,0 +1,8 @@
+from platform import freedesktop_os_release
+import cv2
+cap = cv2.VideoCapture(0)
+while True:
+    _, frame = cap.read()
+    cv2.imshow('Virtual Mouse', frame)
+    cv2.waitKey(1)
+    
